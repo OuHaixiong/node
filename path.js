d450dd53e00d323189e2fb3c63923d665f1fb129
Bear <258333309@163.com>
2016-11-11 18:31:50 +0800

    文件路径

--- path.js
+++ path.js
@@ -17,3 +17,67 @@ console.log(joinPath); // foo/bar
 var extnamePath = path.extname('foo/bar.js'); // 获取文件路径中的扩展名
 console.log(extnamePath); // .js
 
+
+// 下面对目录进行遍历
+var fs = require('fs'); // 系统文件包
+
+/**
+ * 遍历目录下的所有文件，并返回回调函数处理
+ * @param string dir 需要遍历的文件夹（目录）
+ * @param handleTravel callback 处理回调函数
+ */
+function travel(dir, callback) { // 空文件夹不返回
+    fs.readdirSync(dir).forEach(function (file) { // 这里用到了文件处理，需要引入文件包
+    	var filePath = path.join(dir, file);
+    	if (fs.statSync(filePath).isDirectory()) {
+    		travel(filePath, callback);
+    	} else {
+    		callback(filePath);
+    	}
+    });
+}
+
+var arrFilePath = [];
+var num = 0;
+
+/**
+ * 处理遍历过的文件
+ * @param filePath
+ */
+function handleTravel(filePath) {
+	arrFilePath[num] = filePath;
+	num++;
+}
+
+var dir = path.resolve(__dirname, './'); // 等同path.resolve(__dirname)
+dir = path.join(dir, 'afv'); // 拼接路径
+//console.log(dir); //返回/data/www/nodejs/afv
+travel(dir, handleTravel); // 注意：回调函数不能用字符串进行传，直接传函数名就可以
+// 上面的写法还可以写成： travel(dir, function(filePath) {...});
+console.log(arrFilePath);
+
+
+
+function travel(dir, callback, finish) {
+    fs.readdir(dir, function (err, files) {
+        (function next(i) {
+            if (i < files.length) {
+                var pathname = path.join(dir, files[i]);
+
+                fs.stat(pathname, function (err, stats) {
+                    if (stats.isDirectory()) {
+                        travel(pathname, callback, function () {
+                            next(i + 1);
+                        });
+                    } else {
+                        callback(pathname, function () {
+                            next(i + 1);
+                        });
+                    }
+                });
+            } else {
+                finish && finish();
+            }
+        }(0));
+    });
+}
--- test.js
+++ test.js
@@ -11,7 +11,7 @@ function factorial(n) {
 	} else {
 		return n * factorial(n - 1);
 	}
-}
+} // 陷阱： 使用递归算法编写的代码虽然简洁，但由于每递归一次就产生一次函数调用，在需要优先考虑性能时，需要把递归算法转换为循环算法，以减少函数调用次数
 
 console.log(factorial(5));
 console.log(factorial(6));
