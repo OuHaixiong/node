dd341eac4df428e9e313cfa6da8cd5a36d6958e7
Bear <258333309@163.com>
2016-12-28 15:35:36 +0800

    electron学习&&combine 优化

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 node.js 学习积累 ； 2015-5-12创建
 
 
+全局安装模块：npm install XXX -g
+本目录下安装模块并保存入依赖包配置：npm install XXX --sava/-save-dev  save会修改package.json的dependencies;--save-dev会修改devDependencies
+运行时需要用到的包使用--save[产品、线上]，否则只是测试使用时用--save-dev[开发、线下]
---
+++ server_combine/daemon.js
@@ -0,0 +1,28 @@
+// 守护进程(父进程)：用于开启子进程，并监控子进程在挂掉时重启子进程
+
+var child_process = require('child_process');
+var worker;
+
+/**
+ * 开启并重启子进程（服务器进程）
+ * @param string server 子进程的js文件
+ * @param string config json对象的字符串文件名，.json文件
+ */
+function spawn(server, config) {
+    worker = child_process.spawn('node', [server, config]); // 用nodejs执行一个js文件，即开启一个js文件的进程
+    worker.on('exit', function (code) { // 监控事件，如果worker已经退出，就执行如下程序
+	if (code !== 0) {
+            spawn(server, config);
+	}
+    });
+}
+
+function main(argv) {
+    spawn('server.js', argv[0]);
+    process.on('SIGTERM', function () { // 用户主动退出进程，收到SIGTERM信号时终止服务器进程
+        worker.kill();
+        process.exit(0);
+    });
+}
+
+main(process.argv.slice(2)); // 入口函数；使用node daemon.js config.json启动服务
